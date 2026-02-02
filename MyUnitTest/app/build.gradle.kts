e4c07aa3825a483fbf511f549c8b09ff8f67fa8a
oktaavsm <incdmincs@gmail.com>
2026-02-02 10:39:20 +0700

    update project

--- MyUnitTest/app/build.gradle.kts
+++ MyUnitTest/app/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
 
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.inline)
+
+    androidTestImplementation(libs.androidx.runner)
+    androidTestImplementation(libs.androidx.rules)
 }
