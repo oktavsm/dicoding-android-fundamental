a9f85b59721282dcb4d885b7c997e5d0bfac9d4d
oktaavsm <incdmincs@gmail.com>
2026-02-18 10:49:11 +0700

    update: add View Binding

--- MySettingPreference/app/src/main/java/com/dicoding/mysettingpreference/MainActivity.kt
+++ MySettingPreference/app/src/main/java/com/dicoding/mysettingpreference/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dicoding.mysettingpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportFragmentManager.beginTransaction().add(R.id.setting_holder, MyPreferenceFragment()).commit()
     }
